ea02fa0ab761c6d7e52d507549e1e71f5fa0a114
Kevyn Lebouille <kevyn@opsone.net>
2011-08-05 22:58:16 +0200

    Rename IndexController to HomeController

--- app/controllers/index_controller.rb
+++
@@ -1,5 +0,0 @@
-class IndexController < ApplicationController
-  def index
-    @user = User.new
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Whbab::Application.routes.draw do
-  get "index/index"
-
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
@@ -8,7 +6,7 @@ Whbab::Application.routes.draw do
   resources :army_lists
   resources :army_list_choices
 
-  get 'index/index'
+  get 'home/index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -59,7 +57,7 @@ Whbab::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'index#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
