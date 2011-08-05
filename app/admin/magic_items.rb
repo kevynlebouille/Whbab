9f2f08d0a57ccb3835b21da9813b64bf342d0012
Kevyn Lebouille <kevyn@opsone.net>
2011-08-05 23:06:08 +0200

    Custom Magic Item admin panel

--- app/admin/magic_items.rb
+++ app/admin/magic_items.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register MagicItem do
-  
+  controller do
+    def create
+      create! { new_admin_magic_item_url }
+    end
+  end
+
+  action_item :only => :show do
+    link_to "New Magic Item", new_admin_magic_item_path
+  end
+
+  index do
+    column :id
+    column :army
+    column :magic_item_category
+    column :name
+    column :value_points
+    default_actions
+  end
 end
