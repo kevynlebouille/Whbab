e0b584d7786ecc814a8728c54193c135e8863e25
Kevyn Lebouille <kevyn@opsone.net>
2011-08-30 00:23:22 +0200

    Add flash_messages helper and use it in layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   layout Proc.new { |controller| controller.request.xhr? ? false : 'application' }
+
+  def after_sign_in_path_for(resource_or_scope)
+    army_lists_path
+  end
 end
