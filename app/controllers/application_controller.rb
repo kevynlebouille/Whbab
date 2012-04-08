e37aaf3a521adc4e234c09a72e49f4365b12eae4
Kevyn Lebouille <kevyn@opsone.net>
2012-04-08 20:09:00 +0200

    Adapt Devise redirection after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   layout Proc.new { |controller| controller.request.xhr? ? false : 'application' }
 
   def after_sign_in_path_for(resource_or_scope)
-    army_lists_path
+    if resource_or_scope.is_a? AdminUser
+      admin_dashboard_path
+    else
+      army_lists_path
+    end
   end
 end
