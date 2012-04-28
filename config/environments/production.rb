e5d6fed6de6bd6bc78cea32e7a8462711752445e
Kevyn Lebouille <kevyn@opsone.net>
2012-04-29 01:25:25 +0200

    Add pdf.css to asset pipeline precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Whbab::Application.configure do
   config.action_controller.asset_host = 'http://assets.whbab.com'
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( pdf.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
