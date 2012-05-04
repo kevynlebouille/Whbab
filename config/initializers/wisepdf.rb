f092f6ac9b1ec810ef2dbba7953452873dc32bdf
Kevyn Lebouille <kevyn@opsone.net>
2012-05-04 23:01:29 +0200

    Useless wkhtmltopdf option

--- config/initializers/wisepdf.rb
+++ config/initializers/wisepdf.rb
@@ -1,7 +1,6 @@
 Wisepdf::Configuration.configure do |config|
   config.wkhtmltopdf = "#{Rails.root}/script/wkhtmltopdf"
   config.options = {
-    :encoding => 'utf8',
     :layout => "pdf.html",
     :show_as_html => Rails.env.development?,
   }
