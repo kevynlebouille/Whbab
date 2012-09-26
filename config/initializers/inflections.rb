58f1cc2cc84be15eb5f7813ff763b8119595d309
Kevyn Lebouille <kevyn@opsone.net>
2012-09-26 21:19:56 +0200

    Rails 3.2.8 upgrade

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,14 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-ActiveSupport::Inflector.inflections do |inflect|
-  inflect.irregular 'equipment', 'equipments'
-end
-
 # Add new inflection rules using the following format
 # (all these examples are active by default):
+ActiveSupport::Inflector.inflections do |inflect|
+  # inflect.plural /^(ox)$/i, '\1en'
+  # inflect.singular /^(ox)en/i, '\1'
+  # inflect.irregular 'person', 'people'
+  # inflect.uncountable %w( fish sheep )
+  inflect.irregular 'equipment', 'equipments'
+end
+#
+# These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
+#   inflect.acronym 'RESTful'
 # end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Whbab::Application.config.session_store :cookie_store, :key => '_whbab_session'
+Whbab::Application.config.session_store :cookie_store, key: '_whbab_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
