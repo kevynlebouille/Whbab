22aaa3115fbeeac516658ca64675ed2a5915b7c0
Kevyn Lebouille <kevyn@opsone.net>
2012-09-28 23:20:27 +0200

    Clean use of ActiveAdmin additionnal assets

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,21 +1,17 @@
 jQuery(function($) {
   $('#unit_option_parent_id').clone().attr('id', 'unit_option_parent_id_clone').hide().appendTo('body');
-  $('#unit_option_depend_id').clone().attr('id', 'unit_option_depend_id_clone').hide().appendTo('body');
 
   $('#unit_option_unit_id').change(function() {
     var $unit = $(this).find('option[value=' + this.value + ']');
 
     $('#unit_option_parent_id').html($('#unit_option_parent_id_clone').html());
-    $('#unit_option_depend_id').html($('#unit_option_depend_id_clone').html());
 
-    $('#unit_option_parent_id option, #unit_option_depend_id option').each(function() {
+    $('#unit_option_parent_id option').each(function() {
       var $option = $(this);
 
       if ($unit.html() && $option.html() && $option.html().indexOf($unit.html()) !== 0) {
         $option.remove();
       }
     });
-  });
-
-  $('#unit_option_unit_id').change();
+  }).change();
 });
