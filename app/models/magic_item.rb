c33ae78bb42d03a5b0912344a919e7b05b0ceaa5
Kevyn Lebouille <kevyn@opsone.net>
2012-10-11 21:39:43 +0200

    Can have magic standard whitout value points limitation

--- app/models/magic_item.rb
+++ app/models/magic_item.rb
@@ -8,6 +8,10 @@ class MagicItem < ActiveRecord::Base
   validates_numericality_of :value_points, :greater_than_or_equal_to => 0
 
   scope :available_for, lambda { |army, value_points_limit|
-    where("army_id = :army_id OR (army_id IS NULL AND id NOT IN (SELECT override_id FROM magic_items WHERE army_id = :army_id AND override_id IS NOT NULL))", {:army_id => army}).where("value_points <= ?", value_points_limit).order("value_points DESC", "name")
+    if value_points_limit.nil?
+      where("army_id = :army_id OR (army_id IS NULL AND id NOT IN (SELECT override_id FROM magic_items WHERE army_id = :army_id AND override_id IS NOT NULL))", {:army_id => army}).order("value_points DESC", "name")
+    else
+      where("army_id = :army_id OR (army_id IS NULL AND id NOT IN (SELECT override_id FROM magic_items WHERE army_id = :army_id AND override_id IS NOT NULL))", {:army_id => army}).where("value_points <= ?", value_points_limit).order("value_points DESC", "name")
+    end
   }
 end
