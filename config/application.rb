7aa4c0d1cc1ba7c31df1641d2231d8fe5c59d5fa
Kevyn Lebouille <kevyn@opsone.net>
2013-01-25 16:05:49 +0100

    Bump assets version

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,6 @@ module Whbab
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.8'
+    config.assets.version = '1.9'
   end
 end
