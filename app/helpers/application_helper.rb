f3c6ce8b38ca5219e09e0075dd993a1e53cb73d6
Kevyn Lebouille <kevyn@opsone.net>
2014-05-28 23:35:03 +0200

    User ActiveSupport transliterate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,36 +3,7 @@ module ApplicationHelper
     number_with_precision vp, :strip_insignificant_zeros => true, :delimiter => ' ', :separator => ','
   end
 
-  def remove_accents(str)
-    accents_mapping = {
-      'E' => [200,201,202,203],
-      'e' => [232,233,234,235],
-      'A' => [192,193,194,195,196,197],
-      'a' => [224,225,226,227,228,229,230],
-      'C' => [199],
-      'c' => [231],
-      'O' => [210,211,212,213,214,216],
-      'o' => [242,243,244,245,246,248],
-      'I' => [204,205,206,207],
-      'i' => [236,237,238,239],
-      'U' => [217,218,219,220],
-      'u' => [249,250,251,252],
-      'N' => [209],
-      'n' => [241],
-      'Y' => [221],
-      'y' => [253,255],
-      'AE' => [306],
-      'ae' => [346],
-      'OE' => [188],
-      'oe' => [189]
-    }
-
-    accents_mapping.each do |letter,accents|
-      packed = accents.pack('U*')
-      rxp = Regexp.new("[#{packed}]", nil)
-      str.gsub!(rxp, letter)
-    end
-
-    return str
+  def transliterate(str)
+    return ActiveSupport::Inflector.transliterate(str)
   end
 end
