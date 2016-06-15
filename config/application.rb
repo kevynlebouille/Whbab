42fa129aea30856b3f4ebbef9df73759052a5621
Kevyn Lebouille <kevyn@opsone.net>
2016-06-15 13:49:07 +0200

    Remove old way to load local env vars

--- config/application.rb
+++ config/application.rb
@@ -60,13 +60,5 @@ module Whbab
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.13'
-
-    # Local environment variables
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
   end
 end
